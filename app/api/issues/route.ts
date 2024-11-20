a1e5aec030d3a1ab45f5ef16caf563b62a037b9c
denissirenko <sirenko.denis86@gmail.com>
2024-11-20 20:06:01 +0200

    add setup prisma

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import { z } from "zod";
+
+export const issueSchema = z.object({
+  title: z.string().min(1, "Title is required.").max(255),
+  description: z.string().min(1, "Description is required.").max(65535),
+});
+
+export async function POST(request: NextRequest) {
+  console.log("request", request);
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newIssue = prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
+}
