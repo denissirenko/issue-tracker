51a75b2c3e222c62969460fb85f6e952d44cb903
denissirenko <sirenko.denis86@gmail.com>
2024-11-20 17:25:42 +0200

    add the navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+import { IoIosBug } from "react-icons/io";
+
+const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <IoIosBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(({ label, href }) => (
+          <li key={label}>
+            <Link
+              className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              href={href}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
