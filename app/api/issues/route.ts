915936de65fe81b9f23e10c75450d33480dffeb8
denissirenko <sirenko.denis86@gmail.com>
2024-11-21 12:13:14 +0200

    add API for creating issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,20 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { z } from "zod";
 
-export const issueSchema = z.object({
+export const createIssueSchema = z.object({
   title: z.string().min(1, "Title is required.").max(255),
   description: z.string().min(1, "Description is required.").max(65535),
 });
 
 export async function POST(request: NextRequest) {
-  console.log("request", request);
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = createIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  const newIssue = prisma.issue.create({
+  const newIssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   });
 
