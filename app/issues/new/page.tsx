0e84350ae71d37b7bb9c1008e7f7e0dd078d076e
denissirenko <sirenko.denis86@gmail.com>
2024-11-22 17:29:45 +0200

    show issues

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -20,6 +20,7 @@ const NewIssuePage = () => {
   const [isSubmitting, setSubmitting] = useState(false);
 
   const router = useRouter();
+
   const {
     register,
     control,
@@ -29,6 +30,17 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred.");
+    }
+  });
+
   return (
     <div className="max-w-xl space-y-3">
       {error && (
@@ -36,19 +48,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="max-w-xl space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpected error occurred");
-          }
-        })}
-      >
+      <form className="max-w-xl space-y-3" onSubmit={onSubmit}>
         <TextField.Root placeholder="Title" {...register("title")} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
