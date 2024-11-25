36950cf28f15451ac151c4de77f0729a81fa3049
denissirenko <sirenko.denis86@gmail.com>
2024-11-25 12:15:46 +0200

    disable SSR for loading SimpleMDE editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import { Button, Callout, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -11,7 +10,13 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+import dynamic from "next/dynamic";
+
 import "easymde/dist/easymde.min.css";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
