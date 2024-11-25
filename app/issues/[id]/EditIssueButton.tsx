a781b45716e4667fb87d9f7a715009eca3ffb5a8
denissirenko <sirenko.denis86@gmail.com>
2024-11-25 13:44:15 +0200

    add the edit issue page

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,16 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button, Flex } from "@radix-ui/themes";
+import Link from "next/link";
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Button>
+      <Flex gap="1" align="center">
+        <Pencil2Icon />
+        <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+      </Flex>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
