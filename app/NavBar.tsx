a9b2ee6093d94467c52d24d90a7fc46065795dd2
denissirenko <sirenko.denis86@gmail.com>
2024-11-27 13:36:24 +0200

    add skeleton for avatar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,6 +6,7 @@ import React from "react";
 import { IoIosBug } from "react-icons/io";
 import classNames from "classnames";
 import { useSession } from "next-auth/react";
+import { Skeleton } from "@/app/components";
 import {
   Avatar,
   Box,
@@ -66,7 +67,7 @@ const NavLinks = () => {
 const AuthStatus = () => {
   const { status, data: session } = useSession();
 
-  if (status === "loading") return null;
+  if (status === "loading") return <Skeleton width="3rem" />;
 
   if (status === "unauthenticated")
     return (
