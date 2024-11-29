1c2cb4e9672807e606a3ad50df1f88eed86f790c
denissirenko <sirenko.denis86@gmail.com>
2024-11-29 12:39:17 +0200

    Fix avatar not loading

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -86,6 +86,7 @@ const AuthStatus = () => {
             size="2"
             radius="full"
             className="cursor-pointer"
+            referrerPolicy="no-referrer"
           />
         </DropdownMenu.Trigger>
         <DropdownMenu.Content>
