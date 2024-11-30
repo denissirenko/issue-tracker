dc3a50b66e9c99258e8cc41ff683808c94f3e20d
denissirenko <sirenko.denis86@gmail.com>
2024-11-30 12:05:28 +0200

    Build the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import { Heading } from "@radix-ui/themes";
-import Image from "next/image";
+import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Heading>Hello World</Heading>;
-}
+const Home = async ({ searchParams }: { searchParams: { page: string } }) => {
+  const { page } = await searchParams;
+
+  const currentPage = Number.isNaN(parseInt(page)) ? 1 : parseInt(page);
+
+  return (
+    <>
+      <Heading>Home</Heading>
+      <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />
+    </>
+  );
+};
+
+export default Home;
