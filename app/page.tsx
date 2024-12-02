d6cbb62672fdb6004e01074b2dee2335c58d30dd
denissirenko <sirenko.denis86@gmail.com>
2024-12-02 15:45:10 +0200

    Build the IssueChart component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import { Heading } from "@radix-ui/themes";
-import LatestIssues from "./LatestIssues";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
 const Home = async ({ searchParams }: { searchParams: { page: string } }) => {
-  const { page } = await searchParams;
-
-  const currentPage = Number.isNaN(parseInt(page)) ? 1 : parseInt(page);
-
-  return (
-    <>
-      <Heading mb="4">Home</Heading>
-      <LatestIssues />
-    </>
-  );
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 };
 
 export default Home;
